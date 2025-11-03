bf3b8dd66b8b94632bb9a5f524061ffef1dc6ae2
Alejandro <ale101rosales@gmail.com>
2025-11-02 22:18:10 -0600

    Modulo de crear usuarios

--- src/app/api/usuarios/eliminar-usuario/route.js
+++ src/app/api/usuarios/eliminar-usuario/route.js
@@ -1,27 +1,36 @@
-//Endpoint para eliminar un usuario por su ID de vivienda
-import { NextResponse } from "next/server";
-import {conectarBaseDeDatos} from "@/lib/mongodb";
-import UsuarioVivienda from "@/modelos/UsuarioVivienda";
-
+//Endpoint para eliminar un usuario por su vivienda
+import { conectarBaseDeDatos } from "@/lib/mongodb";
+import Usuario from "@/modelos/Usuario";
+import Vivienda from "@/modelos/Vivienda";
 export async function DELETE(request) {
   try {
     await conectarBaseDeDatos();
-    const { viviendaId } = await request.json();
-    const usuarioEliminado = await UsuarioVivienda.findOneAndDelete({ viviendaId });
-    if (!usuarioEliminado) {
-      return NextResponse.json(
-        { error: "Usuario no encontrado." },
+    const { idVivienda } = await request.json();
+    // Buscar la vivienda por idVivienda
+    const vivienda = await Vivienda.findOne({ idVivienda });
+    if (!vivienda) {
+      return new Response(
+        JSON.stringify({ mensaje: "La vivienda no existe." }),
+        { status: 400 }
+      );
+    }
+    // Eliminar el usuario asociado a la vivienda
+    const resultadoEliminacion = await Usuario.deleteOne({ vivienda: vivienda._id });
+    if (resultadoEliminacion.deletedCount === 0) {
+      return new Response(
+        JSON.stringify({ mensaje: "No se encontró un usuario asociado a la vivienda." }),
         { status: 404 }
       );
     }
-    return NextResponse.json(
-      { message: "Usuario eliminado exitosamente." },
+    return new Response(
+      JSON.stringify({ mensaje: "Usuario eliminado exitosamente." }),
       { status: 200 }
     );
-  } catch (error) {
+  }
+  catch (error) {
     console.error("Error al eliminar el usuario:", error);
-    return NextResponse.json(
-      { error: "Error al eliminar el usuario." },
+    return new Response(
+      JSON.stringify({ mensaje: "Error interno del servidor." }),
       { status: 500 }
     );
   }
